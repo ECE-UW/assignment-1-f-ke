c2ad1be37c82d2ddc464b9dd94535272563ed167
f-ke <55255536+f-ke@users.noreply.github.com>
2019-10-02 23:30:43 +0800

    Update a1ece650.py
    regrade

--- a1ece650.py
+++ a1ece650.py
@@ -1,3 +1,5 @@
+"regrage"
+
 import re
 class Point:
     def __init__(self,x,y):
@@ -15,6 +17,7 @@ class line:
         return self.src
     def getd(self):
         return self.dst
+
 
 
 
@@ -62,16 +65,39 @@ class intersect:
         else:
             return None
 
+
+def reinterline(interline):  # merge some more than one intersection group
+    new_intersect_group = []
+    tmp = []
+
+    for j in range(len(interline) - 1):
+        for i in range(j + 1, len(interline)):
+            if interline[j][0] == interline[i][0] and interline[j][-1] == \
+                    interline[i][-1]:
+                for m in interline[j]:
+                    tmp.append(m)
+                for n in interline[i]:
+                    if n not in tmp:
+                        tmp.append(n)
+                    else:
+                        continue
+                del interline[i]
+                del interline[j]
+                interline.append(sorted(tmp))
+                tmp = []
+                break
+        if j == len(interline) - 2:
+            break
+
+    for i in new_intersect_group:
+        interline.append(i)
+    return interline
+
+
 def getvetex(x):#getvetex from input vetex
     import re
 
-    #s=""
-    # for i in x.strip():
-    #     if i == "(": continue
-    #     if i == ",": continue
-    #     if i == ")": continue
-    #     if i == " ": continue #tomorrow use re to optimize it
-    #     print i "this way  not work when we input negative number
+
     x = x.replace(" ", "")
     x = x.replace(")(", ",")
     x = x.replace(")", "")
@@ -87,6 +113,20 @@ def getvetex(x):#getvetex from input vetex
 
     # print("vertex is: ", vertex)
     return vertex
+def overlap(l1,l2):
+    x1, y1 = l1.src.x, l1.src.y
+    x2, y2 = l1.dst.x, l1.dst.y
+    x3, y3 = l2.src.x, l2.src.y
+    x4, y4 = l2.dst.x, l2.dst.y
+    xden_new = (x3-x1)*(y2-y4) - (x2-x4)*(y3-y1)
+    xden_new_2 = (x4-x1)*(y3-y2) - (x3-x2)*(y4-y1)
+    if xden_new == 0 and xden_new_2 == 0:
+        return True
+    else:
+        return False
+
+
+
 dics = {} #dictionary of street and following vertex
 
 vertex_id = []
@@ -96,53 +136,56 @@ vertex_id = []
 
 def main():
     while True:
-        command=raw_input("please put your command here:")
-        if command.strip()=='g':
+        command = raw_input()
+        command_list=[]
+
+
+        if '"' in command:
+            command_list = command.split('"')
+            if len(command_list)==3 and command[0]!='r':
+                vertexstr = getvetex(command_list[2])
+                for i in vertexstr:
+                    if i not in vertex_id:
+                        vertex_id.append(i)
+
+        if command == '':
+            print "Error: empty input!"
+        elif command[0] == 'g':
             Graph()
-            break
-
-        patterna = r'\s*a \"(.+?)\"(( ?\(\-?\d+,\-?\d+\))+)\s*$'
-        patternc = r'\s*c\s*\"(.+?)\"(( ?\(\-?\d+,\-?\d+\))+)\s*$'
-        patternr = r'\s*r\s* \"(.+?)\"'
-        amatch = re.match(patterna, command)
-
-        cmatch = re.match(patternc, command)
-        rmatch = re.match(patternr, command)
-        command=command.replace(") (",")(")
-
-        command_list = command.split('"')
-
-        if len(command_list)==3:
-            vertexstr = getvetex(command_list[2])
-            for i in vertexstr:
-                if i not in vertex_id:
-                    vertex_id.append(i)
-            print "vertex_id:",vertex_id
-            print "command_list:",command_list
-        if amatch:
-            if command_list[1].lower not in dics:
-                dics[command_list[1].lower()]=vertexstr
-
+        elif command[0] == 'a':
+            patterna = r'a\s* \"(.+?)\" (( ?\(\-?\d+,\-?\d+\))+)\s*$'
+            amatch = re.match(patterna, command)
+            if amatch == None:
+                print "Error: invalid input!"
             else:
-                print("errow for street name")
-        elif cmatch:
-            if command_list[1] in dics:
-                dics[command_list[1].lower()]=vertexstr
+                vetstre = getvetex(command_list[2])
+                if command_list[1].lower() not in dics:
+                    dics[command_list[1].lower()] = vetstre
+                else:
+                    print "Error: street name already exists!"
+        elif command[0] == 'c':
+            patternc = r'c\s* \"(.+?)\" (( ?\(\-?\d+,\-?\d+\))+)\s*$'
+            cmatch = re.match(patternc, command)
+            if cmatch == None:
+                print "Error: invalid input! Please check again!"
             else:
-                print("thie street is not existed")
-
-        elif rmatch:
-            if command_list[1] in dics:
-                del dics[command_list[1].lower()]
+                vetstre = getvetex(command_list[2])
+                if command_list[1].lower() in dics:
+                    dics[command_list[1].lower()] =vetstre
+                else:
+                    print "Error: 'c' specified for a street that does not exist!"
+        elif command[0] == 'r':
+            patternr = r'r\s* \"(.+?)\"'
+            rmatch = re.match(patternr, command)
+            if rmatch == None:
+                print "Error: invalid input!"
             else:
-                print "this street has been removed before"
-
-
-
-
-        else:print "informal input"
-        # print "your dics imformation:",dics
-
+                if command_list[1].lower() in dics:
+                    del dics[command_list[1].lower()]
+                else:
+                    print "Error: 'r' specified for a street that does not exist!"
+        else:
+            print "Error: invalid input!"
 
 def Graph():
     global dics
@@ -160,7 +203,7 @@ def Graph():
                 for p in range(len(allpoints[j])-1):
                     l2 = line(Point(allpoints[j][p][0], allpoints[j][p][1]),
                           Point(allpoints[j][p + 1][0], allpoints[j][p + 1][1]))
-                    print l1,l2
+                    #print l1,l2
 
 
                     if intersect(l1,l2).__str__():
@@ -172,6 +215,22 @@ def Graph():
                             interline.append([allpoints[i][m],interpoint,allpoints[i][m+1]])
                         if [allpoints[j][p],interpoint,allpoints[j][p+1]] not in interline:
                             interline.append([allpoints[j][p],interpoint,allpoints[j][p+1]])
+                    elif overlap(l1, l2):
+                        four_nodes = [allpoints[i][m], allpoints[i][m + 1], allpoints[j][p],allpoints[j][p + 1]]
+                        four_nodes = sorted(four_nodes)
+                        interline.append([four_nodes[0], four_nodes[1], four_nodes[2], four_nodes[3]])
+                    else:
+                        continue
+    tmp = []
+
+    while (not (tmp == interline)):
+        tmp = []
+        tmp = tmp + interline
+        interline = reinterline(interline)
+
+
+
+
 
     V={}
     vv=[]
@@ -179,52 +238,50 @@ def Graph():
         for j in interline[i]:
             if j not in vv:
                 vv.append(j)
-    #print "vv:",vv
+
 
     for i in vv:
-
-        # print "vertexid.index(i):",vertex_id.index(i)
-        # print i
         V[vertex_id.index(i)]=i
     #print 'vertex_id:', vertex_id
 
     print "V{"
     for key, value in V.items():
-        print ' ' + str(key) + ': ' + str(value)
+        print ' ' + str(key) + ': ' + '(' + str("{0:.2f}".format(value[0])) + ',' + str(
+            "{0:.2f}".format(value[1])) + ')'
     print "}"
-    for p in range(len(interline)-1):
-        for q in range(p+1, len(interline)):
-            if interline[p][0]==interline[q][0] and interline[p][-1]==interline[q][-1]:
-                newline=[]
-                for i in interline[p]:
-                    newline.append(i)
-                for j in interline[q]:
-                    if j not in newline:
-                        newline.append(j)
-                newline=sorted(newline)
-                del interline[q]
-                del interline[p]
-                interline.append(newline)
-                break
 
-        if p == len(interline) - 2:
-                break
 
-    E=[]
+
+
+
     print "E={"
-    for i in interline:
-        # print'i',i
-        for j in range(len(i)-1):
-            # print 'i[j]',i[j]
-            print "<"+str(vertex_id.index(i[j]))+" "+str(vertex_id.index(i[j+1]))+">"
-            E.append([vertex_id.index(i[j]),vertex_id.index(i[j+1])])
+    for i in range(len(interline)):
+    # print'i',i
+        if i==len(interline)-1:
+
+            for j in range(len(interline[i]) - 2):
+        # print 'i[j]',i[j]
+                print "<" + str(vertex_id.index(interline[i][j])) + " " + str(vertex_id.index(interline[i][j + 1])) +\
+                      ">,"
+            for j in range ((len(interline[i]) - 2),(len(interline[i]) - 1)):
+                print "<" + str(vertex_id.index(interline[i][j])) + " " + str(vertex_id.index(interline[i][j + 1])) +\
+                      ">"
+        else:
+            for j in range(len(interline[i]) - 1):
+        # print 'i[j]',i[j]
+                print "<" + str(vertex_id.index(interline[i][j])) + " " + str(vertex_id.index(interline[i][j + 1])) +\
+                      ">,"
+
+
+
 
     print "}"
-
 
 
 if __name__ == '__main__':
     main()
+
+
 
 
 
